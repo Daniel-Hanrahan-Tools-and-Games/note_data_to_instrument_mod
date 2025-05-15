b89fc698c20da73c737316ce9e3b2b64254a95dc
Daniel-Hanrahan-Tools-and-Games <106982630+Daniel-Hanrahan-Tools-and-Games@users.noreply.github.com>
2025-05-14 22:31:14 -0400

    Add files via upload

---
+++ note_data_to_instrument_mod.lua
@@ -0,0 +1,515 @@
+-- Notice and Name string
+strNoticeAndName = "note_data_to_instrument_mod" .. "\n" .. "Copyright (C) 2025 Daniel Hanrahan Tools and Games" .. "\n" .. "This Expansion or Mod is under the BSD-3-Clause license \n"
+
+-- all functions return note name when called and each function corresponds to note
+
+function Note0()
+    return "C-1"
+end
+
+function Note1()
+    return "C#-1/Db-1"
+end
+
+function Note2()
+    return "D-1"
+end
+
+function Note3()
+    return "D#-1/Eb-1"
+end
+
+function Note4()
+    return "E-1"
+end
+
+function Note5()
+    return "F-1"
+end
+
+function Note6()
+    return "F#-1/Gb-1"
+end
+
+function Note7()
+    return "G-1"
+end
+
+function Note8()
+    return "G#-1/Ab-1"
+end
+
+function Note9()
+    return "A-1"
+end
+
+function Note10()
+    return "A#-1/Bb-1"
+end
+function Note11()
+    return "B-1"
+end
+
+function Note12()
+    return "C0"
+end
+
+function Note13()
+    return "C#0/Db0"
+end
+
+function Note14()
+    return "D0"
+end
+
+function Note15()
+    return "D#0/Eb0"
+end
+
+function Note16()
+    return "E0"
+end
+
+function Note17()
+    return "F0"
+end
+
+function Note18()
+    return "F#0/Gb0"
+end
+
+function Note19()
+    return "G0"
+end
+
+function Note20()
+    return "G#0/Ab0"
+end
+
+function Note21()
+    return "A0"
+end
+
+function Note22()
+    return "A#0/Bb0"
+end
+
+function Note23()
+    return "B0"
+end
+
+function Note24()
+    return "C1"
+end
+
+function Note25()
+    return "C#1/Db1"
+end
+
+function Note26()
+    return "D1"
+end
+
+function Note27()
+    return "D#1/Eb1"
+end
+
+function Note28()
+    return "E1"
+end
+
+function Note29()
+    return "F1"
+end
+
+function Note30()
+    return "F#1/Gb1"
+end
+
+function Note31()
+    return "G1"
+end
+
+function Note32()
+    return "G#1/Ab1"
+end
+
+function Note33()
+    return "A1"
+end
+
+function Note34()
+    return "A#1/Bb1"
+end
+
+function Note35()
+    return "B1"
+end
+
+function Note36()
+    return "C2"
+end
+
+function Note37()
+    return "C#2/Db2"
+end
+
+function Note38()
+    return "D2"
+end
+
+function Note39()
+    return "D#2/Eb2"
+end
+
+function Note40()
+    return "E2"
+end
+
+function Note41()
+    return "F2"
+end
+
+function Note42()
+    return "F#2/Gb2"
+end
+
+function Note43()
+    return "G2"
+end
+
+function Note44()
+    return "G#2/Ab2"
+end
+
+function Note45()
+    return "A2"
+end
+
+function Note46()
+    return "A#2/Bb2"
+end
+
+function Note47()
+    return "B2"
+end
+
+function Note48()
+    return "C3"
+end
+
+function Note49()
+    return "C#3/Db3"
+end
+
+function Note50()
+    return "D3"
+end
+
+function Note51()
+    return "D#3/Eb3"
+end
+
+function Note52()
+    return "E3"
+end
+
+function Note53()
+    return "F3"
+end
+
+function Note54()
+    return "F#3/Gb3"
+end
+
+function Note55()
+    return "G3"
+end
+
+function Note56()
+    return "G#3/Ab3"
+end
+
+function Note57()
+    return "A3"
+end
+
+function Note58()
+    return "A#3/Bb3"
+end
+
+function Note59()
+    return "B3"
+end
+
+function Note60()
+    return "C4 (middle C)"
+end
+
+function Note61()
+    return "C#4/Db4"
+end
+
+function Note62()
+    return "D4"
+end
+
+function Note63()
+    return "D#4/Eb4"
+end
+
+function Note64()
+    return "E4"
+end
+
+function Note65()
+    return "F4"
+end
+
+function Note66()
+    return "F#4/Gb4"
+end
+
+function Note67()
+    return "G4"
+end
+
+function Note68()
+    return "G#4/Ab4"
+end
+
+function Note69()
+    return "A4 concert pitch"
+end
+
+function Note70()
+    return "A#4/Bb4"
+end
+
+function Note71()
+    return "B4"
+end
+
+function Note72()
+    return "C5"
+end
+
+function Note73()
+    return "C#5/Db5"
+end
+
+function Note74()
+    return "D5"
+end
+
+function Note75()
+    return "D#5/Eb5"
+end
+
+function Note76()
+    return "E5"
+end
+
+function Note77()
+    return "F5"
+end
+
+function Note78()
+    return "F#5/Gb5"
+end
+
+function Note79()
+    return "G5"
+end
+
+function Note80()
+    return "G#5/Ab5"
+end
+
+function Note81()
+    return "A5"
+end
+
+function Note82()
+    return "A#5/Bb5"
+end
+
+function Note83()
+    return "B5"
+end
+
+function Note84()
+    return "C6"
+end
+
+function Note85()
+    return "C#6/Db6"
+end
+
+function Note86()
+    return "D6"
+end
+
+function Note87()
+    return "D#6/Eb6"
+end
+
+function Note88()
+    return "E6"
+end
+
+function Note89()
+    return "F6"
+end
+
+function Note90()
+    return "F#6/Gb6"
+end
+
+function Note91()
+    return "G6"
+end
+
+function Note92()
+    return "G#6/Ab6"
+end
+
+function Note93()
+    return "A6"
+end
+
+function Note94()
+    return "A#6/Bb6"
+end
+
+function Note95()
+    return "B6"
+end
+
+function Note96()
+    return "C7"
+end
+
+function Note97()
+    return "C#7/Db7"
+end
+
+function Note98()
+    return "D7"
+end
+
+function Note99()
+    return "D#7/Eb7"
+end
+
+function Note100()
+    return "E7"
+end
+
+function Note101()
+    return "F7"
+end
+
+function Note102()
+    return "F#7/Gb7"
+end
+
+function Note103()
+    return "G7"
+end
+
+function Note104()
+    return "G#7/Ab7"
+end
+
+function Note105()
+    return "A7"
+end
+
+function Note106()
+    return "A#7/Bb7"
+end
+
+function Note107()
+    return "B7"
+end
+
+function Note108()
+    return "C8"
+end
+
+function Note109()
+    return "C#8/Db8"
+end
+
+function Note110()
+    return "D8"
+end
+
+function Note111()
+    return "D#8/Eb8"
+end
+
+function Note112()
+    return "E8"
+end
+
+function Note113()
+    return "F8"
+end
+
+function Note114()
+    return "F#8/Gb8"
+end
+
+function Note115()
+    return "G8"
+end
+
+function Note116()
+    return "G#8/Ab8"
+end
+
+function Note117()
+    return "A8"
+end
+
+function Note118()
+    return "A#8/Bb8"
+end
+
+function Note119()
+    return "B8"
+end
+
+function Note120()
+    return "C9"
+end
+
+function Note121()
+    return "C#9/Db9"
+end
+
+function Note122()
+    return "D9"
+end
+
+function Note123()
+    return "D#9/Eb9"
+end
+
+function Note124()
+    return "E9"
+end
+
+function Note125()
+    return "F9"
+end
+
+function Note126()
+    return "F#9/Gb9"
+end
+
+function Note127()
+    return "G9"
+end
